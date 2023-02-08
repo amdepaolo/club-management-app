7aaecd8398b21b9c17cecdeb888d8bf3c87856f3
Adam DePaolo <amdepaolo@gmail.com>
2023-02-08 16:09:11 -0500

    adds user controller and routes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+
+    def show 
+        user = User.find(params[:id])
+        render json: user
+    end
+
+    def create
+        user = User.create(user_params)
+        render json: user, status: :created
+    end
+
+    private
+
+    def user_params
+        params.permit( :email, :first_name, :last_name, :image, :bio, :password, :password_confirmation)
+    end
+end
