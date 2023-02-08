abd9c79b10356f653f61dc8b0f4f88b097b63f6d
Adam DePaolo <amdepaolo@gmail.com>
2023-02-08 14:45:28 -0500

    migration adds email to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_06_230412) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_08_194416) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_06_230412) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
 end
