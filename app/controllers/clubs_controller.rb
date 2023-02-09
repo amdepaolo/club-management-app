c5b851c2ace2a2b7f3b9b45c7aee61d7ab0e1030
Adam DePaolo <amdepaolo@gmail.com>
2023-02-09 18:24:41 -0500

    creates routes and controller for club

---
+++ app/controllers/clubs_controller.rb
@@ -0,0 +1,35 @@
+class ClubsController < ApplicationController
+
+    def index
+        clubs = Club.all
+        render json: clubs
+    end
+
+    def show
+        club = Club.find(params[:id])
+        render json: club 
+    end
+
+    def create
+        club = Club.create(club_params)
+        render json: club, status: :created
+    end
+
+    def update
+        club = Club.find(params[:id])
+        club.update(club_params)
+        render json: club, status: :accepted
+    end
+
+    def destroy
+        club = Club.find(params[:id])
+        club.destroy
+        head :no_content
+    end
+
+    private
+
+    def club_params
+        params.permit(:name, :description, :meeting_area, :meeting_time, :max_membership)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # root "articles#index"
   get '/hello', to: 'application#hello_world'
   resources :users, only: [:create]
+  resources :clubs 
   get '/me', to: 'users#show'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
