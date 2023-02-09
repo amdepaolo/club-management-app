65d590fba552ea9f640a7e58a02ee65d2ff14186
Adam DePaolo <amdepaolo@gmail.com>
2023-02-08 22:28:25 -0500

    further refines login and signup components

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,29 @@
 import React, {useState} from 'react'
 
 function Login(){
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    function handleSubmit(e){
+        e.preventDefault()
+        console.log({email: email})
+    }
 
     return (
         <div>
             <h2>Log-in</h2>
-            <form>
-                <input type='text' placeholder='email'></input>
-                <input type='text' placeholder='password'></input>
-                <input type='submit'></input>
+            <form onSubmit={handleSubmit}>
+                <input  
+                    type='text' 
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                    placeholder='email'></input>
+                <input
+                    type='password'
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                    placeholder='password'></input>
+                <input type='submit' placeholder='Log-In'></input>
             </form>
         </div>
     )
--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,16 +1,45 @@
 import React, {useState} from "react";
 
 function SignUp(){
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmation, setConfirmation] = useState('')
+
+    function handleSubmit(e){
+        e.preventDefault()
+        console.log({first: firstName, last: lastName, email: email})
+    }
 
     return(
         <div>
             <h2>Sign-Up:</h2>
-            <form>
-                <input type='text' placeholder="email"></input>
-                <input type='text' placeholder="First Name"></input>
-                <input type='text' placeholder="Last Name"></input>
-                <input type='password' placeholder="Password"></input>
-                <input type='password' placeholder="Confirm Password"></input>
+            <form onSubmit={handleSubmit}>
+                <input 
+                    type='text'
+                    value={email} 
+                    onChange={e => setEmail(e.target.value)} placeholder="email"></input>
+                <input 
+                    type='text' 
+                    value={firstName} 
+                    onChange={e => setFirstName(e.target.value)} 
+                    placeholder="First Name"></input>
+                <input 
+                    type='text' 
+                    value={lastName} 
+                    onChange={e => setLastName(e.target.value)}
+                    placeholder="Last Name"></input>
+                <input 
+                    type='password' 
+                    value={password} 
+                    onChange={e => setPassword(e.target.value)}
+                    placeholder="Password"></input>
+                <input 
+                    type='password' 
+                    value={confirmation}
+                    onChange={e => setConfirmation(e.target.value)} 
+                    placeholder="Confirm Password"></input>
                 <input type='submit'></input>
             </form>
         </div>
