128376803664587d9b5f8fcc79fc82b424668c69
Adam DePaolo <amdepaolo@gmail.com>
2023-02-09 15:35:24 -0500

    Adds logout button

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,23 @@ import Login from "./components/Login";
 
 function App() {
   const [user, setUser] = useState(null)
-  useEffect(()=>{
-    fetch('/me')
-  })
+  // useEffect(()=>{
+  //   fetch('/me')
+  // })
+
+  function logOut(){
+    fetch('/logout', {method: 'DELETE'})
+    .then(r => {if (r.ok) 
+      setUser(null)}
+    )
+  }
 
   return (
     <div className="App">
       <h1>Club Connection!</h1>
       <p> This is a placeholder, the app will come later and will look better</p>
       {user? <p>User logged in</p>: <p>No User logged in</p>}
+      <button onClick={logOut}>Log Out</button>
       <Login onLogin={setUser}></Login>
       <SignUp></SignUp>
     </div>
