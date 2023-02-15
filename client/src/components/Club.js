ff82db76018959a881452eed471de2d279630964
Adam DePaolo <amdepaolo@gmail.com>
2023-02-15 17:27:02 -0500

    Adds join club button

--- client/src/components/Club.js
+++ client/src/components/Club.js
@@ -2,12 +2,24 @@ import React from "react";
 
 function Club({club}){
 
+    function handleJoin(){
+        fetch('/clubs/'+club.id+'/memberships', {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            }
+          })
+          .then(r => r.json())
+          .then(r => console.log("Membership created:", r))
+    }
+
     return(
         <div>
             <h3>{club.name}</h3>
             <p>Meeting Time: {club.meeting_time}</p>
             <p>Location: {club.meeting_area}</p>
             <p>{club.description}</p>
+            <button onClick={handleJoin}>Join Club</button>
         </div>
     )
 }
