c617e50e4d34ff4946478b3ffdcd2f067aa4fe7c
Adam DePaolo <amdepaolo@gmail.com>
2023-02-16 15:35:55 -0500

    Adds validator to prevent double membership

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,4 +1,5 @@
 class Membership < ApplicationRecord
     belongs_to :user
     belongs_to :club
+    validates :user_id, uniqueness: {scope: :club_id, message: "already a member of that club"}
 end
