bbb0c8fcb88855ae9e1ba259c809eeb8c60ad706
Adam DePaolo <amdepaolo@gmail.com>
2023-02-16 18:41:25 -0500

    adds current_memberships method

--- app/models/club.rb
+++ app/models/club.rb
@@ -1,4 +1,8 @@
 class Club < ApplicationRecord
     has_many :memberships
     has_many :users, through: :memberships
+
+    def current_memberships
+        self.memberships.length
+    end
 end
--- app/serializers/club_serializer.rb
+++ app/serializers/club_serializer.rb
@@ -1,5 +1,5 @@
 class ClubSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :meeting_area, :meeting_time, :member_id
+  attributes :id, :name, :description, :meeting_area, :meeting_time, :member_id, :current_memberships, :max_membership
 
   def member_id
     membership = self.object.memberships.find_by(user_id: instance_options[:user_id])
