08cdddc62d265b89bd0921988ceaef16d7ee88e6
Adam DePaolo <amdepaolo@gmail.com>
2023-03-27 18:10:49 -0400

    adds email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
+    validates :email, presence: true
     has_many :memberships, dependent: :destroy
     has_many :clubs, through: :memberships
 end
