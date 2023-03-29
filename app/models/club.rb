be46557851231209dcfa978cf0aabb51bfb897c1
Adam DePaolo <amdepaolo@gmail.com>
2023-03-29 18:39:05 -0400

    adds valadation for club

--- app/models/club.rb
+++ app/models/club.rb
@@ -1,6 +1,11 @@
 class Club < ApplicationRecord
     has_many :memberships, dependent: :destroy
     has_many :users, through: :memberships
+    validates :name, presence: true
+    validates :meeting_area, presence: true
+    validates :meeting_time, presence: true
+    validates :max_membership, presence: true
+    validates :description, presence: true
     belongs_to :admin, :class_name => "User", :foreign_key => "admin_id"
 
     def current_memberships
